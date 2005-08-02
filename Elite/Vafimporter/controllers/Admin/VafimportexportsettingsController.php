155ef659895ae93fb4f894fa2c4d0a3af8b45cba
root <root@923a8852-75ef-4272-858b-d33465a0fa2a>
2005-08-02 14:55:12 +0000

    import/export settings
    git-svn-id: svn+ssh://192.168.2.4/svn/vaf-new/trunk@424 923a8852-75ef-4272-858b-d33465a0fa2a

--- Elite/Vafimporter/controllers/Admin/VafimportexportsettingsController.php
+++ Elite/Vafimporter/controllers/Admin/VafimportexportsettingsController.php
@@ -8,12 +8,31 @@ class Elite_Vafimporter_Admin_VafimportexportsettingsController extends Mage_Adm
         $this->loadLayout();
         $this->_setActiveMenu('vaf/import');
         
-        //$this->myIndexAction();
+        $this->myIndexAction();
         
         $block = $this->getLayout()->createBlock('adminhtml/vafimporter_settings', 'vafimporter/settings' );
         
         $this->_addContent( $block );
         $this->renderLayout();
+    }
+    
+    function myIndexAction()
+    {
+        if($_SERVER['REQUEST_METHOD'])
+        {
+            $form = new Elite_Vafimporter_Model_Settings;
+            $form->populate($_POST);
+            print_r($form->getValues());
+            
+            $config = $form->getConfig();
+            $config->importer->allowMissingFields = $form->getValue('allowMissingFields');
+            $config->importer->Y2KMode = $form->getValue('Y2KMode');
+            $config->importer->Y2KThreshold = $form->getValue('Y2KThreshold');
+            
+            $writer = new Zend_Config_Writer_Ini(array('config'   => $config,
+                                                    'filename' => ELITE_CONFIG));
+            echo $writer->write();
+        }
     }
     
     
