c7b6fb9fff63603e8bc1074addd6e6df2368b1ac
root <root@923a8852-75ef-4272-858b-d33465a0fa2a>
2005-08-02 20:51:50 +0000

    0000830: Logo module doesn't work if don't have level called "make" 0000846: Should be able to specify file extension for logo
    git-svn-id: svn+ssh://192.168.2.4/svn/vaf-new/trunk@295 923a8852-75ef-4272-858b-d33465a0fa2a

--- Elite/Vaflogo/Block/Logo.php
+++ Elite/Vaflogo/Block/Logo.php
@@ -1,39 +1,81 @@
 <?php
+
 class Elite_Vaflogo_Block_Logo extends Mage_Core_Block_Abstract
 {
-	function _toHtml()
-	{
-		if( Elite_Vaf_Helper_Data::getInstance()->getConfig()->logo->disable )
+
+    /** @var Zend_Config */
+    protected $config;
+
+    function getConfig()
+    {
+        if( !$this->config instanceof Zend_Config )
         {
-            return;
+            $this->config = Elite_Vaf_Helper_Data::getInstance()->getConfig();
         }
-        if(!$this->selectionPart())
-        {
-			return;
-        }
-        $pathSuffix = $this->selectionToken().'.PNG';
-		return '<img class="vafLogo" src="/logos/' . $pathSuffix .'" style="width:100%" />';
-		
-	}
-	
-	function selectionPart()
+        return $this->config;
+    }
+
+    function setConfig( Zend_Config $config )
+    {
+        $this->config = $config;
+    }
+
+    function _toHtml()
+    {
+	if (Elite_Vaf_Helper_Data::getInstance()->getConfig()->logo->disable)
 	{
-		$vehicle = Elite_Vaf_Helper_Data::getInstance()->getFit();
-		if( !$vehicle )
-		{
-			return false;
-		}
-		$make = $vehicle->getLevel('make');
-		if(!$make)
-		{
-			return false;
-		}
-		return $make->__toString();
+	    return;
 	}
-	
-	function selectionToken()
+	if (!$this->selectionPart())
 	{
-		$selectionPart = $this->selectionPart();
-		return basename(strtoupper(str_replace(' ', '-',$selectionPart)));
+	    return;
 	}
+	$pathSuffix = $this->selectionToken() . '.' . $this->extension();
+	return '<img class="vafLogo" src="/logos/' . $pathSuffix . '" style="width:100%" />';
+    }
+
+    function extension()
+    {
+	if($this->getConfig()->logo->extension)
+	{
+	    return $this->getConfig()->logo->extension;
+	}
+	return 'PNG';
+    }
+
+    function selectionPart()
+    {
+	$vehicle = Elite_Vaf_Helper_Data::getInstance()->getFit();
+	if (!$vehicle)
+	{
+	    return false;
+	}
+
+	if($this->getConfig()->logo->level)
+	{
+	    $level = $this->getConfig()->logo->level;
+	    return $vehicle->getLevel($level);
+	}
+
+	$schema = new Elite_Vaf_Model_Schema;
+	if($schema->getRootLevel() == 'make')
+	{
+	    $make = $vehicle->getLevel('make');
+	    if (!$make)
+	    {
+		return false;
+	    }
+	    return $make->__toString();
+	}
+
+	$rootLevel = $schema->getRootLevel();
+	return $vehicle->getLevel($rootLevel);
+    }
+
+    function selectionToken()
+    {
+	$selectionPart = $this->selectionPart();
+	return basename(strtoupper(str_replace(' ', '-', $selectionPart)));
+    }
+
 }
