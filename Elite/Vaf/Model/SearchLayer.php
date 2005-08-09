9b00cbd606278d57479c78375d9d3d3f9143c1a7
root <root@923a8852-75ef-4272-858b-d33465a0fa2a>
2005-08-09 18:23:52 +0000

    catalogsearch
    git-svn-id: svn+ssh://192.168.2.4/svn/vaf-new/trunk@447 923a8852-75ef-4272-858b-d33465a0fa2a

--- Elite/Vaf/Model/SearchLayer.php
+++ Elite/Vaf/Model/SearchLayer.php
@@ -26,9 +26,11 @@ class Elite_Vaf_Model_SearchLayer extends Mage_CatalogSearch_Model_Layer
             ->addTaxPercents()
             ->addStoreFilter()
             ->addUrlRewrite();
-
-        Mage::getSingleton('catalog/product_status')->addVisibleFilterToCollection($collection);
-        Mage::getSingleton('catalog/product_visibility')->addVisibleInSearchFilterToCollection($collection);
+        
+        if(Mage::helper('catalogsearch')->getQuery()->getQueryText())
+        {
+            $collection->addSearchFilter(Mage::helper('catalogsearch')->getQuery()->getQueryText());
+        }
         
         $ids = Elite_Vaf_Helper_Data::getInstance()->getProductIds();
         if($ids)
@@ -36,15 +38,9 @@ class Elite_Vaf_Model_SearchLayer extends Mage_CatalogSearch_Model_Layer
             $collection->addIdFilter($ids);
         }
         
-        $collection->addAttributeToSelect('name');        
+        Mage::getSingleton('catalog/product_status')->addVisibleFilterToCollection($collection);
+        Mage::getSingleton('catalog/product_visibility')->addVisibleInSearchFilterToCollection($collection);
 
-        if(Mage::helper('catalogsearch')->getQuery()->getQueryText())
-        {
-            $collection->addFieldToFilter(array(
-                    array('attribute'=>'name','like'=>'%'.Mage::helper('catalogsearch')->getQuery()->getQueryText().'%')
-            ));
-        }
-        
         return $this;
     }
         
