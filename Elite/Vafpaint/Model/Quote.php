8c0da3914184b9a216876814ada3dc286768de97
root <root@923a8852-75ef-4272-858b-d33465a0fa2a>
2005-08-10 01:32:13 +0000

    0000861: Can't checkout (paint codes module only)
    git-svn-id: svn+ssh://192.168.2.4/svn/vaf-new/trunk@313 923a8852-75ef-4272-858b-d33465a0fa2a

--- Elite/Vafpaint/Model/Quote.php
+++ Elite/Vafpaint/Model/Quote.php
@@ -1,7 +1,9 @@
 <?php
+
 class Elite_Vafpaint_Model_Quote extends Mage_Sales_Model_Quote
 {
-	/**
+
+    /**
      * Adding new item to quote
      *
      * @param   Mage_Sales_Model_Quote_Item $item
@@ -9,30 +11,28 @@ class Elite_Vafpaint_Model_Quote extends Mage_Sales_Model_Quote
      */
     function addItem(Mage_Sales_Model_Quote_Item $item)
     {
-        // elite paint
-    	if( isset( $_POST['paint'] ))
-    	{
-    		if( 'other' != $_POST['paint'] )
-    		{
-        		$item->setElitePaint( $_POST['paint'] );
-			}
-			else
-			{
-				$item->setElitePaintOther( $_POST['vafPaintCustom'] );
-			}
-            
-		}
-		// elite paint
-		
-        $item->setQuote($this);
-        if (!$item->getId()) {
-            $this->getItemsCollection()->addItem($item);
-            Mage::dispatchEvent('sales_quote_add_item', array('quote_item' => $item));
-        }
-        $this->save();
-        return $this;
+	// elite paint
+	if (isset($_POST['paint']))
+	{
+	    if ('other' != $_POST['paint'])
+	    {
+		$item->setElitePaint($_POST['paint']);
+	    } else
+	    {
+		$item->setElitePaintOther($_POST['vafPaintCustom']);
+	    }
+	}
+	// elite paint
+
+	$item->setQuote($this);
+	if (!$item->getId())
+	{
+	    $this->getItemsCollection()->addItem($item);
+	    Mage::dispatchEvent('sales_quote_add_item', array('quote_item' => $item));
+	}
+	return $this;
     }
-    
+
     /**
      * Retrieve quote item by product id
      *
@@ -41,21 +41,23 @@ class Elite_Vafpaint_Model_Quote extends Mage_Sales_Model_Quote
      */
     function getItemByProduct($product)
     {
-        foreach ($this->getAllItems() as $item) {
-        	// start elite paint
-        	if( isset($_POST['paint']) )
-	        {
-				if( $item->getElitePaint() != $_POST['paint'] )
-				{
-					return false;
-				}
-	        }
-	        // end elite paint
-            if ($item->representProduct($product)) {
-                return $item;
-            }
-        }
-        return false;
+	foreach ($this->getAllItems() as $item)
+	{
+	    // start elite paint
+	    if (isset($_POST['paint']))
+	    {
+		if ($item->getElitePaint() != $_POST['paint'])
+		{
+		    return false;
+		}
+	    }
+	    // end elite paint
+	    if ($item->representProduct($product))
+	    {
+		return $item;
+	    }
+	}
+	return false;
     }
 
 }
