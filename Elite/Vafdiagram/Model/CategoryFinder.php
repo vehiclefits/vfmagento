af0311249fba5dd6991cb1678c10a7c24119faf5
root <root@923a8852-75ef-4272-858b-d33465a0fa2a>
2005-08-11 22:39:26 +0000

    vafdiagram - filters categories
    git-svn-id: svn+ssh://192.168.2.4/svn/vaf-new/trunk@229 923a8852-75ef-4272-858b-d33465a0fa2a

--- Elite/Vafdiagram/Model/CategoryFinder.php
+++ Elite/Vafdiagram/Model/CategoryFinder.php
@@ -1,11 +1,12 @@
 <?php
 class Elite_Vafdiagram_Model_CategoryFinder
 {
-	function listCategories1($paramaters)
+	function listCategories($paramaters)
 	{
 		$select = $this->getReadAdapter()->select()
 			->from('elite_product_servicecode')
 			->where('product_id = ?', $paramaters['product']);
+		$this->filterByParents($select, $paramaters);
 		if(isset($paramaters['service_code']))
 		{
 			$select->where('service_code = ?',$paramaters['service_code']);
@@ -14,9 +15,25 @@ class Elite_Vafdiagram_Model_CategoryFinder
 		$return = array();
 		foreach($rs as $result)
 		{
-			$return[] = $result['category1_id'];
+			$return[] = $this->getCategoryIdFromResult($result,$paramaters);
 		}
 		return $return;
+	}
+	
+	function getCategoryIdFromResult($result,$paramaters)
+	{
+		return $result['category' . $paramaters['level'] . '_id'];		
+	}
+	
+	function filterByParents($select,$paramaters)
+	{
+		for($level=1; $level<=4; $level++)
+		{
+			if(isset($paramaters['level'.$level]))
+			{
+				$select->where('category'.$level.'_id =?', $paramaters['level'.$level]);
+			}
+		}
 	}
 	
 	/** @return Zend_Db_Statement_Interface */
--- Elite/Vafdiagram/Model/CategoryFinderTest.php
+++ Elite/Vafdiagram/Model/CategoryFinderTest.php
@@ -1,7 +1,7 @@
 <?php 
 class Elite_Vafdiagram_Model_CategoryFinderTest extends Elite_Vafdiagram_Model_TestCase
 {
-	function testShouldFilterByProduct()
+	function testShouldFilterByProduct_Level1()
 	{
 		$productId1 = $this->insertProduct('sku1');
 		$productId2 = $this->insertProduct('sku2');
@@ -12,13 +12,14 @@ class Elite_Vafdiagram_Model_CategoryFinderTest extends Elite_Vafdiagram_Model_T
 		
 		$finder = new Elite_Vafdiagram_Model_CategoryFinder;
 		
-		$actual = $finder->listCategories1(array(
+		$actual = $finder->listCategories(array(
+			'level' => 1,
 			'product'=>$productId1
 		));
 		$this->assertEquals( array(5), $actual, 'should filter categories by product');
 	}
 	
-	function testShouldFilterByServiceCode()
+	function testShouldFilterByServiceCode_Level1()
 	{
 		$productId1 = $this->insertProduct('sku1');
 		
@@ -28,10 +29,66 @@ class Elite_Vafdiagram_Model_CategoryFinderTest extends Elite_Vafdiagram_Model_T
 		
 		$finder = new Elite_Vafdiagram_Model_CategoryFinder;
 		
-		$actual = $finder->listCategories1(array(
+		$actual = $finder->listCategories(array(
+			'level' => 1,
 			'product'=>$productId1,
 			'service_code'=>123
 		));
 		$this->assertEquals( array(5), $actual, 'should filter categories by service code');
 	}
+	
+	function testShouldFilterByProduct_Level2()
+	{
+		$productId1 = $this->insertProduct('sku1');
+		$productId2 = $this->insertProduct('sku2');
+		
+		$this->importProductServiceCodes('sku,category1,category2,category3,category4,illustration_id,service_code' . "\n" .
+										 'sku1,5,2,3,4,1234,123' . "\n" . 
+										 'sku2,6,3,3,5,4567,456');		
+		
+		$finder = new Elite_Vafdiagram_Model_CategoryFinder;
+		
+		$actual = $finder->listCategories(array(
+			'level' => 2,
+			'product'=>$productId1
+		));
+		$this->assertEquals( array(2), $actual, 'should filter categories by product');
+	}
+	
+	function testShouldFilterByServiceCode_Level2()
+	{
+		$productId1 = $this->insertProduct('sku1');
+		
+		$this->importProductServiceCodes('sku,category1,category2,category3,category4,illustration_id,service_code' . "\n" .
+										 'sku1,5,2,3,4,1234,123' . "\n" . 
+										 'sku1,6,3,3,5,4567,456');		
+		
+		$finder = new Elite_Vafdiagram_Model_CategoryFinder;
+		
+		$actual = $finder->listCategories(array(
+			'level' => 2,
+			'product'=>$productId1,
+			'service_code'=>123
+		));
+		$this->assertEquals( array(2), $actual, 'should filter categories by service code');
+	}
+	
+	function testShouldFilterByLevel1_Level2()
+	{
+		$productId1 = $this->insertProduct('sku1');
+		
+		$this->importProductServiceCodes('sku,category1,category2,category3,category4,illustration_id,service_code' . "\n" .
+										 'sku1,5,2,3,4,1234,123' . "\n" . 
+										 'sku1,6,3,3,5,1234,1234');		
+		
+		$finder = new Elite_Vafdiagram_Model_CategoryFinder;
+		
+		$actual = $finder->listCategories(array(
+			'level' => 2,
+			'level1' => 5,
+			'product'=>$productId1,
+		));
+		$this->assertEquals( array(2), $actual, 'should filter categories by category level1');
+	}
+
 }
