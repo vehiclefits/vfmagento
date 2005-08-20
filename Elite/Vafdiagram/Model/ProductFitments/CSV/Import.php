9af6abff8440f8534d984fe6e407545f41eca985
root <root@923a8852-75ef-4272-858b-d33465a0fa2a>
2005-08-20 07:13:22 +0000

    vafdiagram
    git-svn-id: svn+ssh://192.168.2.4/svn/vaf-new/trunk@246 923a8852-75ef-4272-858b-d33465a0fa2a

--- Elite/Vafdiagram/Model/ProductFitments/CSV/Import.php
+++ Elite/Vafdiagram/Model/ProductFitments/CSV/Import.php
@@ -1,75 +1,78 @@
 <?php
+
 class Elite_Vafdiagram_Model_ProductFitments_CSV_Import extends Elite_Vafimporter_Model_ProductFitments_CSV_Import
 {
-	function insertRowsIntoTempTable()
+
+    function insertRowsIntoTempTable()
     {
-        $this->cleanupTempTable();
-        while( $row = $this->getReader()->getRow() )
-        {
-            $this->row_number++;
-            
-            $values = $this->getLevelsArray( $row ); 
-            if(!$values)
-            {
-                continue;
-            }
-            
-            $combinations = $this->getCombinations($values, $row);
-            
-            foreach( $combinations as $combination )
-            {
-            	$serviceCode = $this->getFieldValue('service_code', $row);
-            	foreach($this->serviceCodeCombinations($combination, $serviceCode) as $serviceCodeCombination)
-            	{
-					$this->insertIntoTempTable($row, $serviceCodeCombination);            		
-            	}
-            }
-        }
+	$this->cleanupTempTable();
+	while ($row = $this->getReader()->getRow())
+	{
+	    $this->row_number++;
+
+	    $values = $this->getLevelsArray($row);
+	    if (!$values)
+	    {
+		continue;
+	    }
+
+	    $combinations = $this->getCombinations($values, $row);
+
+	    foreach ($combinations as $combination)
+	    {
+		$serviceCode = $this->getFieldValue('service_code', $row);
+		foreach ($this->serviceCodeCombinations($combination, $serviceCode) as $serviceCodeCombination)
+		{
+		    $this->insertIntoTempTable($row, $serviceCodeCombination);
+		}
+	    }
+	}
     }
-    
+
     function updateProductIdsInTempTable()
     {
-    	
+
     }
 
     function insertVehicleRecords()
     {
-        $cols = $this->getSchema()->getLevels();
-        foreach($cols as $i=>$col)
-        {
-            $cols[$i] = $this->getReadAdapter()->quoteIdentifier($col.'_id');
-        }
-        $cols[] = 'service_code';
-        $query = 'REPLACE INTO elite_definition (' . implode(',', $cols) . ')';
-        $query .= ' SELECT DISTINCT ' . implode(',', $cols) . ' FROM elite_import WHERE universal != 1';
+	$cols = $this->getSchema()->getLevels();
+	foreach ($cols as $i => $col)
+	{
+	    $cols[$i] = $this->getReadAdapter()->quoteIdentifier($col . '_id');
+	}
+	$cols[] = 'service_code';
+	$query = 'REPLACE INTO elite_definition (' . implode(',', $cols) . ')';
+	$query .= ' SELECT DISTINCT ' . implode(',', $cols) . ' FROM elite_import WHERE universal != 1';
 
-        $this->query($query);
+	$this->query($query);
     }
-    
-	function serviceCodeCombinations($combination, $serviceCode)
+
+    function serviceCodeCombinations($combination, $serviceCode)
     {
-    	$combinations = array();
-    	foreach($this->products($serviceCode) as $product_id)
-    	{
-    		$combinations[] = $combination + array(
-    			'product_id' => $product_id,
-    			'service_code' => $serviceCode,
-    		);
-    	}
-    	return $combinations;
+	$combinations = array();
+	foreach ($this->products($serviceCode) as $product_id)
+	{
+	    $combinations[] = $combination + array(
+		'product_id' => $product_id,
+		'service_code' => $serviceCode,
+	    );
+	}
+	return $combinations;
     }
-    
+
     function products($serviceCode)
     {
-    	$rs = $this->getReadAdapter()->select()
-    		->from('elite_product_servicecode', array('product_id'))
-    		->where('elite_product_servicecode.service_code = ?', $serviceCode)
-    		->query();
-    	$products = array();
-    	foreach($rs->fetchAll() as $row)
-    	{
-    		$products[] = $row['product_id'];
-    	}
-    	return $products;
+	$rs = $this->getReadAdapter()->select()
+			->from('elite_product_servicecode', array('product_id'))
+			->where('elite_product_servicecode.service_code = ?', $serviceCode)
+			->query();
+	$products = array();
+	foreach ($rs->fetchAll() as $row)
+	{
+	    $products[] = $row['product_id'];
+	}
+	return $products;
     }
+
 }
