44e3ed73b77ec2352a8bae256b78cef035cea819
root <root@923a8852-75ef-4272-858b-d33465a0fa2a>
2005-08-29 23:14:28 +0000

    0000796: [Other] Parts explosion diagram module - new.
    git-svn-id: svn+ssh://192.168.2.4/svn/vaf-new/trunk@256 923a8852-75ef-4272-858b-d33465a0fa2a

--- Elite/Vafdiagram/Model/ProductFinder.php
+++ Elite/Vafdiagram/Model/ProductFinder.php
@@ -1,46 +1,69 @@
-<?php 
+<?php
+
 class Elite_Vafdiagram_Model_ProductFinder
 {
-	function listProductIds($paramaters)
-	{
-		$select = $this->getReadAdapter()->select()
-			->from('elite_product_servicecode', array('product_id'))
-			->where('category1_id = ?', $paramaters['category1']);
-		if(isset($paramaters['category2']))
-		{
-			$select->where('category2_id = ?', $paramaters['category2']);
-		}
-		if(isset($paramaters['category3']))
-		{
-			$select->where('category3_id = ?', $paramaters['category3']);
-		}
-		if(isset($paramaters['category4']))
-		{
-			$select->where('category4_id = ?', $paramaters['category4']);
-		}
-		if(isset($paramaters['service_code']))
-		{
-			$select->where('service_code = ?', $paramaters['service_code']);
-		}
-		$select->order('callout');
-		$rs = $select->query()->fetchAll();
-		$return = array();
-		foreach($rs as $result)
-		{
-			$return[] = $result['product_id'];
-		}
-		return $return;
-	}
-	
-	/** @return Zend_Db_Statement_Interface */
-    protected function query( $sql )
+
+    function listProductIds($paramaters)
     {
-        return $this->getReadAdapter()->query( $sql );
+	$select = $this->getReadAdapter()->select()
+			->from('elite_product_servicecode', array('product_id'));
+	$this->filter($select,$paramaters);
+	$select->order('callout');
+	$rs = $select->query()->fetchAll();
+	$return = array();
+	foreach ($rs as $result)
+	{
+	    $return[] = $result['product_id'];
+	}
+	return $return;
     }
-    
+
+    function listIllustrationIds($paramaters)
+    {
+	$select = $this->getReadAdapter()->select()
+			->from('elite_product_servicecode', array('distinct(illustration_id)'));
+	$this->filter($select,$paramaters);
+	$select->order('callout');
+	$rs = $select->query()->fetchAll();
+	$return = array();
+	foreach ($rs as $result)
+	{
+	    $return[] = $result['illustration_id'];
+	}
+	return $return;
+    }
+
+    function filter($select,$paramaters)
+    {
+	$select->where('category1_id = ?', $paramaters['category1']);
+	if (isset($paramaters['category2']))
+	{
+	    $select->where('category2_id = ?', $paramaters['category2']);
+	}
+	if (isset($paramaters['category3']))
+	{
+	    $select->where('category3_id = ?', $paramaters['category3']);
+	}
+	if (isset($paramaters['category4']))
+	{
+	    $select->where('category4_id = ?', $paramaters['category4']);
+	}
+	if (isset($paramaters['service_code']))
+	{
+	    $select->where('service_code = ?', $paramaters['service_code']);
+	}
+    }
+
+    /** @return Zend_Db_Statement_Interface */
+    protected function query($sql)
+    {
+	return $this->getReadAdapter()->query($sql);
+    }
+
     /** @return Zend_Db_Adapter_Abstract */
     protected function getReadAdapter()
     {
-        return Elite_Vaf_Helper_Data::getInstance()->getReadAdapter();
+	return Elite_Vaf_Helper_Data::getInstance()->getReadAdapter();
     }
+
 }
--- Elite/Vafdiagram/Model/ProductServiceCodeImporterTest.php
+++ Elite/Vafdiagram/Model/ProductServiceCodeImporterTest.php
@@ -1,100 +1,103 @@
 <?php
+
 class Elite_Vafdiagram_Model_ProductServiceCodeImporterTest extends Elite_Vafdiagram_Model_TestCase
 {
-	function doSetUp()
-	{
-		$this->switchSchema('make,model,year');
-	}
-	
-	function testShouldImportServiceCodes()
-	{
-		$productId = $this->insertProduct('sku1');
-		
-		$this->importProductServiceCodes('sku,service_code' . "\n" .
-										 'sku1,123' . "\n" . 
-										 'sku1,456');
-		$product = $this->product($productId);		
-		$this->assertEquals( array(123,456), $product->serviceCodes(), 'should import multiple service codes for product');
-	}
-	
-	function testShouldImportCallout()
-	{
-		$productId = $this->insertProduct('sku1');
-		
-		$this->importProductServiceCodes('sku,category1,service_code,callout' . "\n" .
-										 'sku1,1,123,1' . "\n" . 
-										 'sku1,2,456,2');
-		$product = $this->product($productId);		
-		$this->assertEquals( 1, $product->callout(array('category1'=>1)), 'should import callout');
-		$this->assertEquals( 2, $product->callout(array('category1'=>2)), 'should import callout');
-	}
-	
-	function testShouldAssociateCategory1WithServiceCode()
-	{
-		$productId = $this->insertProduct('sku1');
-		
-		$this->importProductServiceCodes('sku,category1,service_code' . "\n" .
-										 'sku1,1,123' . "\n" . 
-										 'sku1,2,456');
-		$product = $this->product($productId);		
-		$this->assertEquals( array(123), $product->serviceCodes(array('category1'=>1)), 'should assocaite category 1 with product service codes');
-		$this->assertEquals( array(456), $product->serviceCodes(array('category1'=>2)), 'should assocaite category 1 with product service codes');
-	}
-	
-	function testShouldAssociateCategory2WithServiceCode()
-	{
-		$productId = $this->insertProduct('sku1');
-		
-		$this->importProductServiceCodes('sku,category1,category2,service_code' . "\n" .
-										 'sku1,1,1,123' . "\n" . 
-										 'sku1,1,2,456');
-		$product = $this->product($productId);		
-		$this->assertEquals( array(123), $product->serviceCodes(array('category1'=>1,'category2'=>1)), 'should assocaite category 2 with product service codes');
-		$this->assertEquals( array(456), $product->serviceCodes(array('category1'=>1,'category2'=>2)), 'should assocaite category 2 with product service codes');
-	}
-	
-	function testShouldAssociateCategory3WithServiceCode()
-	{
-		$productId = $this->insertProduct('sku1');
-		
-		$this->importProductServiceCodes('sku,category1,category2,category3,service_code' . "\n" .
-										 'sku1,1,1,1,123' . "\n" . 
-										 'sku1,1,1,2,456');
-		$product = $this->product($productId);		
-		$this->assertEquals( array(123), $product->serviceCodes(array('category1'=>1,'category2'=>1,'category3'=>1)), 'should assocaite category 3 with product service codes');
-		$this->assertEquals( array(456), $product->serviceCodes(array('category1'=>1,'category2'=>1,'category3'=>2)), 'should assocaite category 3 with product service codes');
-	}
-	
-	function testShouldAssociateCategory4WithServiceCode()
-	{
-		$productId = $this->insertProduct('sku1');
-		
-		$this->importProductServiceCodes('sku,category1,category2,category3,category4,service_code' . "\n" .
-										 'sku1,1,1,1,1,123' . "\n" . 
-										 'sku1,1,1,1,2,456');
-		$product = $this->product($productId);		
-		$this->assertEquals( array(123), $product->serviceCodes(array('category1'=>1,'category2'=>1,'category3'=>1,'category4'=>1)), 'should assocaite category 4 with product service codes');
-		$this->assertEquals( array(456), $product->serviceCodes(array('category1'=>1,'category2'=>1,'category3'=>1,'category4'=>2)), 'should assocaite category 4 with product service codes');
-	}
-	
-	function testShouldAssociateCategory1WithIllustration()
-	{
-		$productId = $this->insertProduct('sku1');
-		
-		$this->importProductServiceCodes('sku,category1,illustration_id,service_code' . "\n" .
-										 'sku1,1,1234,123' . "\n" . 
-										 'sku1,2,4567,456');
-		$product = $this->product($productId);	
 
-		$actual = $product->illustrationId(array(
-			'category1'=>1,
-			'service_code'=>123
+    function doSetUp()
+    {
+	$this->switchSchema('make,model,year');
+    }
+
+    function testShouldImportServiceCodes()
+    {
+	$productId = $this->insertProduct('sku1');
+
+	$this->importProductServiceCodes('sku,service_code' . "\n" .
+		'sku1,123' . "\n" .
+		'sku1,456');
+	$product = $this->product($productId);
+	$this->assertEquals(array(123, 456), $product->serviceCodes(), 'should import multiple service codes for product');
+    }
+
+    function testShouldImportCallout()
+    {
+	$productId = $this->insertProduct('sku1');
+
+	$this->importProductServiceCodes('sku,category1,service_code,callout' . "\n" .
+		'sku1,1,123,1' . "\n" .
+		'sku1,2,456,2');
+	$product = $this->product($productId);
+	$this->assertEquals(1, $product->callout(array('category1' => 1)), 'should import callout');
+	$this->assertEquals(2, $product->callout(array('category1' => 2)), 'should import callout');
+    }
+
+    function testShouldAssociateCategory1WithServiceCode()
+    {
+	$productId = $this->insertProduct('sku1');
+
+	$this->importProductServiceCodes('sku,category1,service_code' . "\n" .
+		'sku1,1,123' . "\n" .
+		'sku1,2,456');
+	$product = $this->product($productId);
+	$this->assertEquals(array(123), $product->serviceCodes(array('category1' => 1)), 'should assocaite category 1 with product service codes');
+	$this->assertEquals(array(456), $product->serviceCodes(array('category1' => 2)), 'should assocaite category 1 with product service codes');
+    }
+
+    function testShouldAssociateCategory2WithServiceCode()
+    {
+	$productId = $this->insertProduct('sku1');
+
+	$this->importProductServiceCodes('sku,category1,category2,service_code' . "\n" .
+		'sku1,1,1,123' . "\n" .
+		'sku1,1,2,456');
+	$product = $this->product($productId);
+	$this->assertEquals(array(123), $product->serviceCodes(array('category1' => 1, 'category2' => 1)), 'should assocaite category 2 with product service codes');
+	$this->assertEquals(array(456), $product->serviceCodes(array('category1' => 1, 'category2' => 2)), 'should assocaite category 2 with product service codes');
+    }
+
+    function testShouldAssociateCategory3WithServiceCode()
+    {
+	$productId = $this->insertProduct('sku1');
+
+	$this->importProductServiceCodes('sku,category1,category2,category3,service_code' . "\n" .
+		'sku1,1,1,1,123' . "\n" .
+		'sku1,1,1,2,456');
+	$product = $this->product($productId);
+	$this->assertEquals(array(123), $product->serviceCodes(array('category1' => 1, 'category2' => 1, 'category3' => 1)), 'should assocaite category 3 with product service codes');
+	$this->assertEquals(array(456), $product->serviceCodes(array('category1' => 1, 'category2' => 1, 'category3' => 2)), 'should assocaite category 3 with product service codes');
+    }
+
+    function testShouldAssociateCategory4WithServiceCode()
+    {
+	$productId = $this->insertProduct('sku1');
+
+	$this->importProductServiceCodes('sku,category1,category2,category3,category4,service_code' . "\n" .
+		'sku1,1,1,1,1,123' . "\n" .
+		'sku1,1,1,1,2,456');
+	$product = $this->product($productId);
+	$this->assertEquals(array(123), $product->serviceCodes(array('category1' => 1, 'category2' => 1, 'category3' => 1, 'category4' => 1)), 'should assocaite category 4 with product service codes');
+	$this->assertEquals(array(456), $product->serviceCodes(array('category1' => 1, 'category2' => 1, 'category3' => 1, 'category4' => 2)), 'should assocaite category 4 with product service codes');
+    }
+
+    function testShouldAssociateCategory1WithIllustration()
+    {
+	$productId = $this->insertProduct('sku1');
+
+	$this->importProductServiceCodes('sku,category1,illustration_id,service_code' . "\n" .
+		'sku1,1,1234,123' . "\n" .
+		'sku1,2,4567,456');
+	$product = $this->product($productId);
+
+	$actual = $product->illustrationId(array(
+		    'category1' => 1,
+		    'service_code' => 123
 		));
-		$this->assertEquals( 1234, $actual, 'should lookup have correct illustration ID for category & service code combination');
-	}
-	
-	function testShouldAssociateProductIdWithIllustration()
-	{
-		return $this->markTestIncomplete();
-	}
+	$this->assertEquals(1234, $actual, 'should lookup have correct illustration ID for category & service code combination');
+    }
+
+    function testShouldAssociateProductIdWithIllustration()
+    {
+	return $this->markTestIncomplete();
+    }
+
 }
