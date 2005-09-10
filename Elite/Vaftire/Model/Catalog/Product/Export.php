03d439fb606211d5dacae9aed0bb620a6c46af69
root <root@923a8852-75ef-4272-858b-d33465a0fa2a>
2005-09-10 12:38:55 +0000

    tire export - got under test
    git-svn-id: svn+ssh://192.168.2.4/svn/vaf-new/trunk@270 923a8852-75ef-4272-858b-d33465a0fa2a

--- Elite/Vaftire/Model/Catalog/Product/Export.php
+++ Elite/Vaftire/Model/Catalog/Product/Export.php
@@ -1,36 +1,52 @@
 <?php
+
 class Elite_Vaftire_Model_Catalog_Product_Export
 {
-	function export()
-	{
-		$csv = '"sku","section_width","aspect_ratio","diameter"';
-		$csv .= "\n";
-		
-		foreach($this->getCollection() as $product)
-		{
-			$product = new Elite_Vaftire_Model_Catalog_Product($product);
-			if($product->getTireSize())
-			{
-				$tireSize = $product->getTireSize();
-				
-				$csv .= '"'.$product->getSku().'",';
-				$csv .= '"'.$tireSize->sectionWidth().'",';
-				$csv .= '"'.$tireSize->aspectRatio().'",';
-				$csv .= '"'.$tireSize->diameter().'"';
-				$csv .= "\n";
-			}
-		}
-		
-		return $csv;
-	}
-	
-	function getCollection()
+
+    function export()
     {
-        $collection = new Mage_Catalog_Model_Resource_Eav_Mysql4_Product_Collection;
-        $collection->addAttributeToSelect('*');
-        
-        Mage::getSingleton('catalog/product_status')->addVisibleFilterToCollection($collection);
-//        Mage::getSingleton('catalog/product_visibility')->addVisibleInCatalogFilterToCollection($collection);
-        return $collection;
+	$csv = '"sku","section_width","aspect_ratio","diameter"';
+	$csv .= "\n";
+
+	foreach ($this->getProductRows() as $productRow)
+	{
+	    $product = new Elite_Vaf_Model_Catalog_Product;
+	    $product->setId($productRow['entity_id']);
+	    
+	    $product = new Elite_Vaftire_Model_Catalog_Product($product);
+	    if ($product->getTireSize())
+	    {
+		$tireSize = $product->getTireSize();
+
+		$csv .= '"' . $productRow['sku'] . '",';
+		$csv .= '"' . $tireSize->sectionWidth() . '",';
+		$csv .= '"' . $tireSize->aspectRatio() . '",';
+		$csv .= '"' . $tireSize->diameter() . '"';
+		$csv .= "\n";
+	    }
+	}
+
+	return $csv;
     }
+
+    function getProductRows()
+    {
+	$query = Elite_Vaf_Helper_Data::getInstance()->getReadAdapter()->select()
+		->from($this->getProductTable(), array('entity_id','sku'));
+	$rs = $query->query();
+	$ids = array();
+	while($productRow = $rs->fetch())
+	{
+	    array_push($ids, $productRow);
+	}
+	return $ids;
+    }
+
+    function getProductTable()
+    {
+        $resource = new Mage_Catalog_Model_Resource_Eav_Mysql4_Product;
+        $table = $resource->getTable( 'catalog/product' );
+        return $table;
+    }
+
 }
--- Elite/Vaftire/Model/Catalog/Product/ExportTest.php
+++ Elite/Vaftire/Model/Catalog/Product/ExportTest.php
@@ -3,20 +3,35 @@ class Elite_Vaftire_Model_Catalog_Product_ExportTest extends Elite_Vaf_TestCase
 {
 	function testExportHeaders()
 	{
-		return $this->markTestIncomplete();
-		//$tireSize = new Elite_Vaftire_Model_TireSize(205, 55, 16);
-//        
-//        $product = $this->newTireProduct();
-//        $product->setId(1);
-//        $product->setTireSize($tireSize);
-//		
-//		$export = new Elite_Vaftire_Model_Catalog_Product_Export;
-//		$csv = $export->export();
-//		
-//		$expected = '"id","section_width","aspect_ratio","diameter"
-//"1","205","55,"16"
-//';
+	    $tireSize = new Elite_Vaftire_Model_TireSize(205, 55, 16);
 
-//		$this->assertEquals( $expected, $csv );
+	    $id = $this->insertProduct( 'sku123' );
+
+	    $product = $this->newTireProduct();
+	    $product->setId($id);
+	    $product->setTireSize($tireSize);
+
+	    //$stream = fopen("php://temp", 'w');
+	    $export = new Elite_Vaftire_Model_Catalog_Product_ExportTestSub;
+//	    $export->export($stream);
+	    $csv = $export->export();
+
+	    //rewind($stream);
+
+	    //$data = stream_get_contents($stream);
+
+	    $expected = '"sku","section_width","aspect_ratio","diameter"
+"sku123","205","55","16"
+';
+
+	    $this->assertEquals( $expected, $csv );
 	}
+}
+
+class Elite_Vaftire_Model_Catalog_Product_ExportTestSub extends Elite_Vaftire_Model_Catalog_Product_Export
+{
+    function getProductTable()
+    {
+	return 'test_catalog_product_entity';
+    }
 }
