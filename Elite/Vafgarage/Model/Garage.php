8d746d0a2476d9170dbb0b9670bf67e5c9619dd5
root <root@923a8852-75ef-4272-858b-d33465a0fa2a>
2005-09-14 15:06:23 +0000

    0000857: Using Vaflink module adds random cars to the garage
    git-svn-id: svn+ssh://192.168.2.4/svn/vaf-new/trunk@334 923a8852-75ef-4272-858b-d33465a0fa2a

--- Elite/Vafgarage/Model/Garage.php
+++ Elite/Vafgarage/Model/Garage.php
@@ -1,42 +1,35 @@
 <?php
-class Elite_Vafgarage_Model_Garage
-{
-	protected $vehicles = array();
-	
-	function addVehicle($vehicle)
-	{
-		if($this->hasVehicle($vehicle))
-		{
-			return false;
-		}
-		$this->vehicles[] = $vehicle;
-	}
-	
-	function removeVehicle($vehicle)
-	{
-		foreach($this->vehicles() as $key=>$eachVehicle)
-		{
-			if($vehicle === $eachVehicle)
-			{
-				unset($this->vehicles[$key]);
-			}
-		}
-	}
 
-	function hasVehicle($vehicle)
-	{
-		foreach($this->vehicles() as $eachVehicle)
-		{
-			if($vehicle === $eachVehicle)
-			{
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	function vehicles()
-	{
-		return $this->vehicles;
-	}
+class Elite_Vafgarage_Model_Garage {
+
+    protected $vehicles = array();
+
+    function addVehicle($vehicle) {
+        if ($this->hasVehicle($vehicle)) {
+            return false;
+        }
+        $this->vehicles[] = $vehicle;
+    }
+
+    function removeVehicle($vehicle) {
+        foreach ($this->vehicles() as $key => $eachVehicle) {
+            if ($vehicle === $eachVehicle) {
+                unset($this->vehicles[$key]);
+            }
+        }
+    }
+
+    function hasVehicle($vehicle) {
+        foreach ($this->vehicles() as $eachVehicle) {
+            if ($vehicle === $eachVehicle) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function vehicles() {
+        return $this->vehicles;
+    }
+
 }
--- Elite/Vafgarage/Model/GarageTest.php
+++ Elite/Vafgarage/Model/GarageTest.php
@@ -1,27 +1,33 @@
 <?php
-class Elite_Vafgarage_Model_GarageTest extends Elite_Vaf_TestCase
-{
-	function testAddVehicle()
-	{
-		$garage = new Elite_Vafgarage_Model_Garage;
-		$garage->addVehicle(array('make'=>5, 'model'=>3, 'year'=>2));
-		$vehicles = $garage->vehicles();
-		$this->assertEquals(array('make'=>5, 'model'=>3, 'year'=>2), $vehicles[0] );
-	}
 
-	function testOnlyAddsOneOfEachVehicle()
-	{
-		$garage = new Elite_Vafgarage_Model_Garage;
-		$garage->addVehicle(array('make'=>5, 'model'=>3, 'year'=>2));
-		$garage->addVehicle(array('make'=>5, 'model'=>3, 'year'=>2));
-		$this->assertEquals(1, count($garage->vehicles()) );
-	}
-	
-	function testRemoveVehicle()
-	{
-		$garage = new Elite_Vafgarage_Model_Garage;
-		$garage->addVehicle(array('make'=>5, 'model'=>3, 'year'=>2));
-		$garage->removeVehicle(array('make'=>5, 'model'=>3, 'year'=>2));
-		$this->assertEquals(0, count($garage->vehicles()) );
-	}
+class Elite_Vafgarage_Model_GarageTest extends Elite_Vaf_TestCase {
+
+    function testAddVehicle() {
+        $garage = new Elite_Vafgarage_Model_Garage;
+        $garage->addVehicle(array('make' => 5, 'model' => 3, 'year' => 2));
+        $vehicles = $garage->vehicles();
+        $this->assertEquals(array('make' => 5, 'model' => 3, 'year' => 2), $vehicles[0]);
+    }
+
+    function testAddPartialVehicle() {
+        $garage = new Elite_Vafgarage_Model_Garage;
+        $garage->addVehicle(array('make' => 5, 'model' => 0, 'year' => 0));
+        $vehicles = $garage->vehicles();
+        $this->assertEquals(array('make' => 5, 'model' => 0, 'year' => 0), $vehicles[0]);
+    }
+
+    function testOnlyAddsOneOfEachVehicle() {
+        $garage = new Elite_Vafgarage_Model_Garage;
+        $garage->addVehicle(array('make' => 5, 'model' => 3, 'year' => 2));
+        $garage->addVehicle(array('make' => 5, 'model' => 3, 'year' => 2));
+        $this->assertEquals(1, count($garage->vehicles()));
+    }
+
+    function testRemoveVehicle() {
+        $garage = new Elite_Vafgarage_Model_Garage;
+        $garage->addVehicle(array('make' => 5, 'model' => 3, 'year' => 2));
+        $garage->removeVehicle(array('make' => 5, 'model' => 3, 'year' => 2));
+        $this->assertEquals(0, count($garage->vehicles()));
+    }
+
 }
