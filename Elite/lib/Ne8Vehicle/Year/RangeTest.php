f0c8ac9a4f9f715d341db1f04e00dfad5b47bd90
root <root@923a8852-75ef-4272-858b-d33465a0fa2a>
2010-10-22 22:15:45 +0000

    year - organized range tests
    git-svn-id: svn+ssh://192.168.2.4/svn/vaf-new/trunk@14 923a8852-75ef-4272-858b-d33465a0fa2a

--- Elite/lib/Ne8Vehicle/Year/RangeTest.php
+++ Elite/lib/Ne8Vehicle/Year/RangeTest.php
@@ -1,37 +1,13 @@
 <?php
 class Ne8Vehicle_Year_RangeTest extends Elite_Vaf_TestCase
 {
-    function testShouldBeInvalid_BlankString()
-    {
-        $range = new Ne8Vehicle_Year_Range('');
-        $this->assertFalse( $range->isValid(), 'should be invalid when blank string is passed' );
-    }
-    
-    function testShouldBeInvalid_String()
-    {
-        $range = new Ne8Vehicle_Year_Range('foo');
-        $this->assertFalse( $range->isValid(), 'should be invalid when string is passed' );
-    }
-    
-    function testShouldSingle4DigitYearShouldBeValid()
-    {
-        $range = new Ne8Vehicle_Year_Range('2004');
-        $this->assertTrue($range->isValid(), 'single 4 digit year should be valid');
-    }
-    
-    function testShouldSingle4DigitYearShouldBeValid_TrimSpace()
-    {
-        $range = new Ne8Vehicle_Year_Range('2004 ');
-        $this->assertTrue($range->isValid(), 'single 4 digit year should be valid (with trailing space)');
-    }
-    
     function testShouldUseSingle4DigitYearForStartValue()
     {
         $range = new Ne8Vehicle_Year_Range('2004');
         $this->assertEquals( 2004, $range->start(), 'should use single 4 digit year as start year' );
     }
     
-    function testShouldUseSingle4DigitYearForSEndValue()
+    function testShouldUseSingle4DigitYearForEndValue()
     {
         $range = new Ne8Vehicle_Year_Range('2004');
         $this->assertEquals( 2004, $range->end(), 'should use single 4 digit year as end year' );
@@ -49,22 +25,10 @@ class Ne8Vehicle_Year_RangeTest extends Elite_Vaf_TestCase
         $this->assertEquals( 2004, $range->start(), 'should use single 2 digit year as start year' );
     }
     
-    function testShouldUseSingle2DigitYearForSEndValue()
+    function testShouldUseSingle2DigitYearForEndValue()
     {
         $range = new Ne8Vehicle_Year_Range('04');
         $this->assertEquals( 2004, $range->end(), 'should use single 2 digit year as end year' );
-    }
-    
-    function testShouldBeValid_2DigitYears()
-    {
-        $range = new Ne8Vehicle_Year_Range('02-03');
-        $this->assertTrue( $range->isValid(), 'should be valid with 2 digit years' );
-    }
-    
-    function testShouldBeValidWithWhiteSpaces()
-    {
-        $range = new Ne8Vehicle_Year_Range(' 02 - 03 ');
-        $this->assertTrue( $range->isValid(), 'should trim spaces' );
     }
     
     function testShouldBeTrimSpacesOnStartYear()
@@ -77,30 +41,6 @@ class Ne8Vehicle_Year_RangeTest extends Elite_Vaf_TestCase
     {
         $range = new Ne8Vehicle_Year_Range(' 02 - 03 ');
         $this->assertEquals( '03', $range->endInput(), 'should trim spaces on end year' );
-    }
-    
-    function testShouldBeValid_1DigitEndYear()
-    {
-        $range = new Ne8Vehicle_Year_Range('02-3');
-        $this->assertFalse( $range->isValid(), 'should be invalid with 1 digit end year' );
-    }
-    
-    function testShouldBeValid_1DigitStartYear()
-    {
-        $range = new Ne8Vehicle_Year_Range('2-34');
-        $this->assertFalse( $range->isValid(), 'should be invalid with 1 digit start year' );
-    }
-    
-    function test4DigitStartShouldBeValid()
-    {
-        $range = new Ne8Vehicle_Year_Range('2004-');
-        $this->assertTrue( $range->isValid(), '4 digit start year should be valid range' );
-    }
-    
-    function test4DigitEndShouldBeValid()
-    {
-        $range = new Ne8Vehicle_Year_Range('-2005');
-        $this->assertTrue( $range->isValid(), '4 digit end year should be valid range' );
     }
     
     function testShouldGetStart4Digit()
---
+++ Elite/lib/Ne8Vehicle/Year/RangeTests/ValidationTest.php
@@ -0,0 +1,63 @@
+<?php
+class Ne8Vehicle_Year_RangeTests_ValidationTest extends Elite_Vaf_TestCase
+{
+    function testShouldBeInvalid_BlankString()
+    {
+        $range = new Ne8Vehicle_Year_Range('');
+        $this->assertFalse( $range->isValid(), 'should be invalid when blank string is passed' );
+    }
+    
+    function testShouldBeInvalid_String()
+    {
+        $range = new Ne8Vehicle_Year_Range('foo');
+        $this->assertFalse( $range->isValid(), 'should be invalid when string is passed' );
+    }
+    
+    function testShouldSingle4DigitYearShouldBeValid()
+    {
+        $range = new Ne8Vehicle_Year_Range('2004');
+        $this->assertTrue($range->isValid(), 'single 4 digit year should be valid');
+    }
+    
+    function testShouldSingle4DigitYearShouldBeValid_TrimSpace()
+    {
+        $range = new Ne8Vehicle_Year_Range('2004 ');
+        $this->assertTrue($range->isValid(), 'single 4 digit year should be valid (with trailing space)');
+    }
+    
+    function testShouldBeValid_2DigitYears()
+    {
+        $range = new Ne8Vehicle_Year_Range('02-03');
+        $this->assertTrue( $range->isValid(), 'should be valid with 2 digit years' );
+    }
+    
+    function testShouldBeValidWithWhiteSpaces()
+    {
+        $range = new Ne8Vehicle_Year_Range(' 02 - 03 ');
+        $this->assertTrue( $range->isValid(), 'should trim spaces' );
+    }
+    
+    function testShouldBeValid_1DigitEndYear()
+    {
+        $range = new Ne8Vehicle_Year_Range('02-3');
+        $this->assertFalse( $range->isValid(), 'should be invalid with 1 digit end year' );
+    }
+    
+    function testShouldBeValid_1DigitStartYear()
+    {
+        $range = new Ne8Vehicle_Year_Range('2-34');
+        $this->assertFalse( $range->isValid(), 'should be invalid with 1 digit start year' );
+    }
+    
+    function test4DigitStartShouldBeValid()
+    {
+        $range = new Ne8Vehicle_Year_Range('2004-');
+        $this->assertTrue( $range->isValid(), '4 digit start year should be valid range' );
+    }
+    
+    function test4DigitEndShouldBeValid()
+    {
+        $range = new Ne8Vehicle_Year_Range('-2005');
+        $this->assertTrue( $range->isValid(), '4 digit end year should be valid range' );
+    }
+}
