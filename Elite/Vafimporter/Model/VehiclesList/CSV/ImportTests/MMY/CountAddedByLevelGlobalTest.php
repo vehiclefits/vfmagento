86975dace00b98fafc0316c31131f8d3e86497df
root <root@923a8852-75ef-4272-858b-d33465a0fa2a>
2010-10-24 15:04:55 +0000

    importer
    git-svn-id: svn+ssh://192.168.2.4/svn/vaf-new/trunk@25 923a8852-75ef-4272-858b-d33465a0fa2a

---
+++ Elite/Vafimporter/Model/VehiclesList/CSV/ImportTests/MMY/CountAddedByLevelGlobalTest.php
@@ -0,0 +1,48 @@
+<?php
+class Elite_Vafimporter_Model_VehiclesList_CSV_ImportTests_MMY_CountAddedByLevelGlobalTest extends Elite_Vafimporter_TestCase
+{    
+    function doSetUp()
+    {
+        $schemaGenerator = new Elite_Vaf_Model_Schema_Generator();
+        $schemaGenerator->dropExistingTables();
+        $schemaGenerator->execute(array(
+            'year',
+            'make' => array('global'=>true),
+            'model'
+        ));
+        $this->startTransaction();
+    }
+    
+    function doTearDown()
+    {
+        $schemaGenerator = new Elite_Vaf_Model_Schema_Generator();
+        $schemaGenerator->dropExistingTables();
+    }
+    
+    function testShouldAddTwoModels()
+    {
+        $importer = $this->importVehiclesList('make, model, year' . "\n" .
+                                              'honda, integra, 2000' . "\n" .
+                                              'acura, integra, 2000');
+                                              
+        $this->assertEquals( 2, $importer->getCountAddedByLevel('model'), 'should add two models' );
+    }
+    
+    function testShouldAddYear()
+    {
+        $importer = $this->importVehiclesList('make, model, year' . "\n" .
+                                              'acura, integra, 2000');
+                                              
+        $this->assertEquals( 1, $importer->getCountAddedByLevel('year'), 'should add year' );
+    }
+    
+    function testShouldCountUniqueYears()
+    {
+        $importer = $this->importVehiclesList('make, model, year' . "\n" .
+                                              'honda, civic, 2000' . "\n" .
+                                              'acura, integra, 2000');
+                                              
+        $this->assertEquals( 1, $importer->getCountAddedByLevel('year'), 'should count unique years' );
+    }
+    
+}
