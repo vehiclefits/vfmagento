96639cfbb2424b4110c3a8a7c40c139025311e0f
root <root@923a8852-75ef-4272-858b-d33465a0fa2a>
2010-10-24 15:01:00 +0000

    importer - count test
    git-svn-id: svn+ssh://192.168.2.4/svn/vaf-new/trunk@23 923a8852-75ef-4272-858b-d33465a0fa2a

--- Elite/Vafimporter/Model/VehiclesList/CSV/ImportTests/MMY/CountTest.php
+++ Elite/Vafimporter/Model/VehiclesList/CSV/ImportTests/MMY/CountTest.php
@@ -1,75 +1,101 @@
 <?php
 class Elite_Vafimporter_Model_VehiclesList_CSV_ImportTests_MMY_CountTest extends Elite_Vafimporter_TestCase
 {    
-    const MAKES = 2;
-    const MODELS = 3;
-    const YEARS = 11;
-
-    protected $csvData;
-
     function doSetUp()
     {
         $this->switchSchema('make,model,year');
-        $this->csvData = 'make, model, year
-honda, civic, 2000
-honda, civic, 2001
-honda, civic, 2002
-honda, accord, 2003
-honda, accord, 2004
-honda, accord, 2005
-honda, accord, 2006
-acura, integra, 2000
-acura, integra, 2001
-acura, integra, 2002
-acura, integra, 2003';
     }
     
     function testCountAddedDefaults0()
     {
-        $importer = $this->importVehiclesList( $this->csvData );
+        $importer = $this->importVehiclesList('make, model, year' . "\n" .
+                                              'honda, civic, 2000');
+                                              
         $this->assertEquals( 0, $importer->getCountAddedByLevel('foobar'), 'countAdded should always return 0, even if the level was not part of the imported data' );
     }
     
-    function testWhenImportingWillAddMakes()
+    function testShouldAddMake()
     {
-        $importer = $this->importVehiclesList( $this->csvData );
-        $this->assertEquals( self::MAKES, $importer->getCountAddedByLevel('make'), 'When importing Makes not yet import, WILL count them as added' );
-    }
-
-    function testWhenImportingWillAddModels()
-    {
-        $importer = $this->importVehiclesList( $this->csvData );
-        $this->assertEquals( self::MODELS, $importer->getCountAddedByLevel('model'), 'When importing Models not yet import, WILL count them as added' );
+        $importer = $this->importVehiclesList('make, model, year' . "\n" .
+                                              'acura, integra, 2000');
+                                              
+        $this->assertEquals( 1, $importer->getCountAddedByLevel('make'), 'should add make' );
     }
     
-    function testWhenImportingWillAddYears()
+    function testShouldCountUniqueMakes()
     {
-        $importer = $this->importVehiclesList( $this->csvData );
-        $this->assertEquals( self::YEARS, $importer->getCountAddedByLevel('year'), 'When importing Years not yet import, WILL count them as added' );
+        $importer = $this->importVehiclesList('make, model, year' . "\n" .
+                                              'acura, integra, 2000' . "\n" .
+                                              'acura, integra, 2001');
+                                              
+        $this->assertEquals( 1, $importer->getCountAddedByLevel('make'), 'should count unique makes' );
+    }
+    
+    function testShouldAddTwoMakes()
+    {
+        $importer = $this->importVehiclesList('make, model, year' . "\n" .
+                                              'honda, civic, 2000' . "\n" .
+                                              'acura, integra, 2000');
+                                              
+        $this->assertEquals( 2, $importer->getCountAddedByLevel('make'), 'should add two makes' );
+    }
+
+    function testShouldAddModels()
+    {
+        $importer = $this->importVehiclesList('make, model, year' . "\n" .
+                                              'acura, integra, 2000');
+                                              
+        $this->assertEquals( 1, $importer->getCountAddedByLevel('model'), 'should add model' );
+    }
+    
+    function testShouldAddTwoModels()
+    {
+        $importer = $this->importVehiclesList('make, model, year' . "\n" .
+                                              'honda, civic, 2000' . "\n" .
+                                              'acura, integra, 2000');
+                                              
+        $this->assertEquals( 2, $importer->getCountAddedByLevel('model'), 'should add two models' );
+    }
+    
+    function testShouldAddYear()
+    {
+        $importer = $this->importVehiclesList('make, model, year' . "\n" .
+                                              'acura, integra, 2000');
+                                              
+        $this->assertEquals( 1, $importer->getCountAddedByLevel('year'), 'should add year' );
+    }
+    
+    function testShouldAddTwoYears()
+    {
+        $importer = $this->importVehiclesList('make, model, year' . "\n" .
+                                              'honda, civic, 2000' . "\n" .
+                                              'acura, integra, 2000');
+                                              
+        $this->assertEquals( 2, $importer->getCountAddedByLevel('year'), 'should add two years' );
     }
     
     function testWhenNoMakesWillAdd0()
     {
-        $importer = $this->importTwice( $this->csvData );
+        $importer = $this->importVehiclesListTwice( 'make, model, year' . "\n" .
+                                        'acura, integra, 2000' );
+                                        
         $this->assertEquals( 0, $importer->getCountAddedByLevel('make'), 'Importing data we already have WILL NOT count the Make as added' );
     }
     
     function testWhenNoModelsWillAdd0()
     {
-        $importer = $this->importTwice( $this->csvData );
+        $importer = $this->importVehiclesListTwice( 'make, model, year' . "\n" .
+                                        'acura, integra, 2000' );
+                                        
         $this->assertEquals( 0, $importer->getCountAddedByLevel('model'), 'Importing data we already have WILL NOT count the Model as added' );
     }
     
     function testWhenNoYearsWillAdd0()
     {
-        $importer = $this->importTwice( $this->csvData );
+        $importer = $this->importVehiclesListTwice( 'make, model, year' . "\n" .
+                                        'acura, integra, 2000' );
+                                        
         $this->assertEquals( 0, $importer->getCountAddedByLevel('year'), 'Importing data we already have WILL NOT count the Year as added' );
-    }
-    
-    protected function importTwice( $file )
-    {
-        $this->importVehiclesList( $file );
-        return $this->importVehiclesList( $file );
     }
     
 }
