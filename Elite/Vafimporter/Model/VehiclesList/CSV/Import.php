0ad70c77225109ec5c3be92ac66bd76bace6f6af
root <root@923a8852-75ef-4272-858b-d33465a0fa2a>
2010-10-24 18:04:29 +0000

    importer - sprouted 'range exploder' class
    git-svn-id: svn+ssh://192.168.2.4/svn/vaf-new/trunk@36 923a8852-75ef-4272-858b-d33465a0fa2a

--- Elite/Vafimporter/Model/VehiclesList/CSV/Import.php
+++ Elite/Vafimporter/Model/VehiclesList/CSV/Import.php
@@ -419,118 +419,14 @@ class Elite_Vafimporter_Model_VehiclesList_CSV_Import extends Elite_Vafimporter_
     
     function explodeRanges($values,$level)
     {
-        if( isset($values[$level.'_range']))
+        $exploder = new Elite_Vafimporter_Model_RangeExploder($this->getConfig());
+        $values = $exploder->explodeRanges($values,$level);
+        if($exploder->getError())
         {
-            $val = $values[$level.'_range'];
-            unset($values[$level.'_range']);
-            list($start,$end) = $this->explodeRangesOneColumn($val);
+            $this->log( 'Line(' . $this->row_number . ') ' . $exploder->getError(), Zend_Log::NOTICE );
+            return false;
         }
-        else
-        {
-            $start_value = $values[$level.'_start'];
-            $end_value = $values[$level.'_end'];
-            unset($values[$level.'_start']);
-            unset($values[$level.'_end']);
-            list($start,$end) = $this->explodeRangesTwoColumns($start_value, $end_value);
-        }
-        
-        $this->fixYears($start,$end);
-        for( $currentValue = $start; $currentValue <= $end; $currentValue++ )
-        {
-            $values[$level][] = str_pad($currentValue,2,'0',STR_PAD_LEFT);
-        }
-        
         return $values;
-    }
-    
-    function fixYears( &$start, &$end )
-    {
-        if( $start > $end )
-        {
-            $newStart = $start;
-            $newEnd = $end;
-            
-            $start = $newEnd;
-            $end = $newStart;
-        }
-        
-        if( $start && !$end )
-        {
-            $end = $start;
-        }
-        
-        if( !$start && $end )
-        {
-            $start = $end;
-        }
-    }
-    
-    function explodeRangesOneColumn($val)
-    {
-        $range = $this->yearRange($val);
-            
-        if( !$range->isValid() )
-        {
-            $this->log('Line(' . $this->row_number . ') Invalid Year Range: [' . $val . ']', Zend_Log::NOTICE);
-            return false;
-        }
-        
-        $start = $range->start();
-        $end = $range->end();
-        return array($start,$end);
-    }
-    
-    function explodeRangesTwoColumns($start_value,$end_value)
-    {
-        $startYear = $this->year($start_value);
-        $endYear = $this->year($end_value);
-        
-        if( !$startYear->isValid() && !$endYear->isValid() )
-        {
-            $this->log('Line(' . $this->row_number . ') Invalid Year Range: [' . $start_value . '] & [' . $end_value . ']', Zend_Log::NOTICE);
-            return false;
-        }
-        
-        $start = $this->yearValue($startYear);
-        $end = $this->yearValue($endYear);
-        return array($start, $end);
-    }
-    
-    function year($value)
-    {
-        $year = new Ne8Vehicle_Year($value);
-        if(isset($this->getConfig()->importer->Y2KMode) && false === $this->getConfig()->importer->Y2KMode)
-        {
-            $year->setY2KMode(false);
-        }
-        if($this->getConfig()->importer->Y2KThreshold)
-        {
-            $year->setThreshold($this->getConfig()->importer->Y2KThreshold);
-        }
-        return $year;
-    }
-    
-    function yearRange($value)
-    {
-        $range = new Ne8Vehicle_Year_Range($value);
-        if(isset($this->getConfig()->importer->Y2KMode) && false === $this->getConfig()->importer->Y2KMode)
-        {
-            $range->setY2KMode(false);
-        }
-        if($this->getConfig()->importer->Y2KThreshold)
-        {
-            $range->setThreshold($this->getConfig()->importer->Y2KThreshold);
-        }
-        return $range;
-    }
-    
-    function yearValue($year)
-    {
-        if($year->isValid())
-        {
-            return $year->value();
-        }
-        return null;
     }
     
     function getSchema()
