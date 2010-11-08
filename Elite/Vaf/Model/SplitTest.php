2aea49a0527f935314b2f9345ccbbce3799ab644
root <root@923a8852-75ef-4272-858b-d33465a0fa2a>
2010-11-08 15:59:19 +0000

    split tests
    git-svn-id: svn+ssh://192.168.2.4/svn/vaf-new/trunk@168 923a8852-75ef-4272-858b-d33465a0fa2a

--- Elite/Vaf/Model/SplitTest.php
+++ Elite/Vaf/Model/SplitTest.php
@@ -15,6 +15,31 @@ class Elite_Vaf_Model_SplitTest extends Elite_Vaf_TestCase
         $this->assertTrue( $this->vehicleExists(array('make'=>'Ford','model'=>'F-250','year'=>2000)) );
         $this->assertFalse( $this->vehicleExists(array('make'=>'Ford','model'=>'F-150/F-250','year'=>2000)), 'should delete old vehicle' );
     }
+        
+    function testShouldSplitModel_MultipleYears()
+    {
+        $this->createMMY('Ford','F-150/F-250','2001');
+        $vehicle = $this->createMMY('Ford','F-150/F-250','2000');
+        
+        $this->split($vehicle, 'model', array('F-150', 'F-250'));
+        
+        $this->assertTrue( $this->vehicleExists(array('make'=>'Ford','model'=>'F-150','year'=>2000)) );
+        $this->assertTrue( $this->vehicleExists(array('make'=>'Ford','model'=>'F-150','year'=>2001)) );
+        $this->assertTrue( $this->vehicleExists(array('make'=>'Ford','model'=>'F-250','year'=>2000)) );
+        $this->assertTrue( $this->vehicleExists(array('make'=>'Ford','model'=>'F-250','year'=>2001)) );
+        $this->assertFalse( $this->vehicleExists(array('make'=>'Ford','model'=>'F-150/F-250','year'=>2000)), 'should delete old vehicle' );
+        $this->assertFalse( $this->vehicleExists(array('make'=>'Ford','model'=>'F-150/F-250','year'=>2001)), 'should delete old vehicle' );
+    }
+    
+    function testShouldSplitMake()
+    {
+        $vehicle = $this->createMMY('Ford/Ford 2','F-150','2000');
+        
+        $this->split($vehicle, 'make', array('Ford','Ford 2'));
+        
+        $this->assertTrue( $this->vehicleExists(array('make'=>'Ford','model'=>'F-150','year'=>2000)) );
+        $this->assertTrue( $this->vehicleExists(array('make'=>'Ford 2','model'=>'F-150','year'=>2000)) );
+    }
     
     function split($vehicle, $grain, $newTitles)
     {
