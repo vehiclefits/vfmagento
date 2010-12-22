ae4c9198d177aa2aecdc01cc89203cf96cc1dfa6
root <root@923a8852-75ef-4272-858b-d33465a0fa2a>
2010-12-22 13:21:43 +0000

    - 0000790: [Other] Upgrade issue
    git-svn-id: svn+ssh://192.168.2.4/svn/vaf-new/trunk@191 923a8852-75ef-4272-858b-d33465a0fa2a

--- Elite/Vaf/sql/migrations/18_change_universal_notnull.php
+++ Elite/Vaf/sql/migrations/18_change_universal_notnull.php
@@ -6,7 +6,27 @@ class Vaf18
         $schema = new Elite_Vaf_Model_Schema();
         $db = Elite_Vaf_Helper_Data::getInstance()->getReadAdapter();
 
-        $db->query('ALTER TABLE `elite_import` CHANGE `universal` `universal` INT( 1 ) NULL DEFAULT \'0\'');  
+        $db->query('DROP TABLE elite_import');
+        $db->query('
+CREATE TABLE IF NOT EXISTS `elite_import` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `make` varchar(255) NOT NULL,
+  `make_id` int(50) NOT NULL,
+  `model` varchar(255) NOT NULL,
+  `model_id` int(50) NOT NULL,
+  `year` varchar(255) NOT NULL,
+  `year_id` int(50) NOT NULL,
+  `sku` varchar(255) DEFAULT NULL,
+  `product_id` int(255) DEFAULT NULL,
+  `universal` int(1) DEFAULT '0',
+  `existing` int(1) NOT NULL,
+  `line` int(255) NOT NULL,
+  `mapping_id` int(255) NOT NULL,
+  `note_message` varchar(255) NOT NULL,
+  `notes` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;');
+          
         
     }
 }
