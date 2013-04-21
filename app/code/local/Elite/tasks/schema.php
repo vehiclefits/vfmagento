cad371f861bdb23e40a8b05b4ae91e6dd0f34c4b
josh <josh.ribakoff@gmail.com>
2013-04-21 13:33:44 -0400

    fixed schema CLI tool for resetting the schema
    It needed to use the new paths

--- app/code/local/Elite/tasks/schema.php
+++ app/code/local/Elite/tasks/schema.php
@@ -21,7 +21,8 @@
  * @copyright  Copyright (c) 2013 Vehicle Fits, llc
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-require_once( dirname( __FILE__ ) . '/../Vaf/TestSuite.php' );
+require_once( dirname( __FILE__ ).'/config.default.php');
+require_once( dirname( __FILE__ ) . '/../Vaf/bootstrap-tests.php' );
 
 class Schema_CLI
 {
