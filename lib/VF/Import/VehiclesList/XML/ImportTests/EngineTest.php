7f881c62e7bb47d9f8ea0ab29c307127c914639d
josh <josh.ribakoff@gmail.com>
2013-04-20 23:56:24 -0400

    Added license to top of each .php file & changed line endings with dos2linux

--- lib/VF/Import/VehiclesList/XML/ImportTests/EngineTest.php
+++ lib/VF/Import/VehiclesList/XML/ImportTests/EngineTest.php
@@ -1,4 +1,26 @@
 <?php
+/**
+ * Vehicle Fits
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Vehicle Fits to newer
+ * versions in the future. If you wish to customize Vehicle Fits for your
+ * needs please refer to http://www.vehiclefits.com for more information.
+
+ * @copyright  Copyright (c) 2013 Vehicle Fits, llc
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 class VF_Import_VehiclesList_XML_ImportTests_EngineTest extends VF_Import_VehiclesList_XML_TestCase
 {
     protected $csvData;
