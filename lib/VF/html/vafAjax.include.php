812af7a9776e2c62e27a70d8aad4399a00800337
josh <josh.ribakoff@gmail.com>
2013-04-27 19:05:50 -0400

    Removed out of date license comment

--- lib/VF/html/vafAjax.include.php
+++ lib/VF/html/vafAjax.include.php
@@ -21,23 +21,11 @@
  * @copyright  Copyright (c) 2013 Vehicle Fits, llc
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-/**
-* Vehicle Fits Free Edition - Copyright (c) 2008-2010 by Vehicle Fits, LLC
-* PROFESSIONAL IDENTIFICATION:
-* "www.vehiclefits.com"
-* PROMOTIONAL SLOGAN FOR AUTHOR'S PROFESSIONAL PRACTICE:
-* "Automotive Ecommerce Provided By Vehicle Fits llc"
-*
-* All Rights Reserved
-* VEHICLE FITS ATTRIBUTION ASSURANCE LICENSE (adapted from the original OSI license)
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the conditions in license.txt are met
-*/
 class vafAjaxInclude
 {
     function execute()
     {
-        $schema = new VF_Schema();
+        $schema = new VF_Schema();//$_GET['schema']);
         $ajax = new VF_Ajax();
         return $ajax->execute( $schema );   
     }
