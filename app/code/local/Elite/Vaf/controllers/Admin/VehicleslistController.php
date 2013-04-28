ff82ebda2a49fa41848494c09e9e629dea4933e6
josh <josh.ribakoff@gmail.com>
2013-04-28 00:26:09 -0400

    Removed obsolete license comment

--- app/code/local/Elite/Vaf/controllers/Admin/VehicleslistController.php
+++ app/code/local/Elite/Vaf/controllers/Admin/VehicleslistController.php
@@ -21,18 +21,6 @@
  * @copyright  Copyright (c) 2013 Vehicle Fits, llc
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-/**
- * Vehicle Fits Free Edition - Copyright (c) 2008-2010 by Vehicle Fits, LLC
- * PROFESSIONAL IDENTIFICATION:
- * "www.vehiclefits.com"
- * PROMOTIONAL SLOGAN FOR AUTHOR'S PROFESSIONAL PRACTICE:
- * "Automotive Ecommerce Provided By Vehicle Fits llc"
- *
- * All Rights Reserved
- * VEHICLE FITS ATTRIBUTION ASSURANCE LICENSE (adapted from the original OSI license)
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the conditions in license.txt are met
- */
 class Elite_Vaf_Admin_VehicleslistController extends Mage_Adminhtml_Controller_Action
 {
     /** @var Zend_Config */
