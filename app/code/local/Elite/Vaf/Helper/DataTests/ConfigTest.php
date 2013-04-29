ef792adbfaf9d0a9a3bd0f9513ac26f8078a93b5
josh <josh.ribakoff@gmail.com>
2013-04-28 21:25:24 -0400

    deleted Elite_Vaf_Helper_DataTestCase

--- app/code/local/Elite/Vaf/Helper/DataTests/ConfigTest.php
+++ app/code/local/Elite/Vaf/Helper/DataTests/ConfigTest.php
@@ -21,7 +21,7 @@
  * @copyright  Copyright (c) 2013 Vehicle Fits, llc
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class Elite_Vaf_Helper_DataTest_DataConfigTest extends Elite_Vaf_Helper_DataTestCase
+class Elite_Vaf_Helper_DataTest_DataConfigTest extends VF_TestCase
 {
     function testGetConfigSearch()
     {
