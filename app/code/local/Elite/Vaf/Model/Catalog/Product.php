416f751e070bc637e75ba6b91ee5ae65229214ae
josh <josh.ribakoff@gmail.com>
2013-05-03 23:23:24 -0400

    I accidentally renamed the class after running the tests & before commiting, my IDE had focus instead of the terminal. Fixing.

--- app/code/local/Elite/Vaf/Model/Catalog/Product.php
+++ app/code/local/Elite/Vaf/Model/Catalog/Product.php
@@ -20,7 +20,7 @@
  * @copyright  Copyright (c) 2013 Vehicle Fits, llc
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class git extends Mage_Catalog_Model_Product implements VF_Configurable
+class Elite_Vaf_Model_Catalog_Product extends Mage_Catalog_Model_Product implements VF_Configurable
 {
 
     protected $vf_product;
