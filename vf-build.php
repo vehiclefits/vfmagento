c43cad104f75e041404c15e3a5eb625f736bcc0e
josh <josh.ribakoff@gmail.com>
2013-10-13 11:04:13 -0400

    created build script to copy only essential files to /tmp/

---
+++ vf-build.php
@@ -0,0 +1,7 @@
+<?php
+$version = '3.0.0';
+$buildPath = '/tmp/vfmagento-'.$version;
+passthru("rsync -avr --exclude='.idea' --exclude='vendor' --exclude='phpunit.*' --exclude='vf-build.php' --exclude='.git' --exclude='*Test.php' --delete-after . $buildPath");
+passthru("rm -rf $buildPath/app/code/local/Elite/vendor/vehiclefits/library/tests");
+passthru("rm -rf $buildPath/app/code/local/Elite/vendor/joshribakoff/php-csv-utils/tests");
+passthru("cd $buildPath/app/code/local/Elite; composer install");
