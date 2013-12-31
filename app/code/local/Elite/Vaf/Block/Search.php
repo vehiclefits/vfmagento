c13fb2dcb118385456734c4143e17d39ce04a7c2
Kyle Cannon <kyle@dcpowerinc.com>
2013-12-31 09:27:59 -0800

    Reformat code to PSR1/PSR2 on Elite_Vaf_Block_Search

--- app/code/local/Elite/Vaf/Block/Search.php
+++ app/code/local/Elite/Vaf/Block/Search.php
@@ -17,7 +17,7 @@
  * Do not edit or add to this file if you wish to upgrade Vehicle Fits to newer
  * versions in the future. If you wish to customize Vehicle Fits for your
  * needs please refer to http://www.vehiclefits.com for more information.
-
+ *
  * @copyright  Copyright (c) 2013 Vehicle Fits, llc
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
@@ -25,63 +25,59 @@
 class Elite_Vaf_Block_Search extends Elite_Vaf_Block_Abstract implements VF_Configurable
 {
     public $searchStrategy;
-    
+
     function __construct()
     {
         $this->searchStrategy = new Elite_Vaf_Search_Mage();
     }
-    
+
     function __call($method, $arguments)
     {
-        if(method_exists($this->searchStrategy,$method))
-        {
-            return call_user_func_array(array($this->searchStrategy,$method), $arguments);
-        }
-        else
-        {
+        if (method_exists($this->searchStrategy, $method)) {
+            return call_user_func_array(array($this->searchStrategy, $method), $arguments);
+        } else {
             return parent::__call($method, $arguments);
         }
     }
 
-    function url( $route )
+    function url($route)
     {
-        return Mage::getUrl( $route );
+        return Mage::getUrl($route);
     }
 
     function _toHtml()
     {
-        if( !$this->shouldShow( $this->currentCategoryId() ) )
-        {
+        if (!$this->shouldShow($this->currentCategoryId())) {
             return '';
         }
         $html = $this->renderView();
         return $html;
     }
-    
+
     function setTemplate($template)
     {
         $this->searchStrategy->setTemplate($template);
         parent::setTemplate($template);
         return $this;
     }
-    
+
     function setRequest($request)
     {
         $this->request = $request;
         $this->searchStrategy->setRequest($request);
     }
-    
+
     function getRequest()
     {
         return $this->searchStrategy->getRequest();
     }
-    
+
     function getConfig()
     {
         return $this->searchStrategy->getConfig();
     }
 
-    function setConfig( Zend_Config $config )
+    function setConfig(Zend_Config $config)
     {
         $this->searchStrategy->setConfig($config);
     }
